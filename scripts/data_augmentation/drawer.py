0fb01dfc4517344b617bc5c10b3ff703b216eb3b
hibikid39 <ojikan39@gmail.com>
2022-09-28 07:51:32 +0000

    add draw_bubble

--- scripts/data_augmentation/drawer.py
+++ scripts/data_augmentation/drawer.py
@@ -29,31 +29,56 @@ def draw_lines(image, points):
     for point_pair in points:
         pair_start = (point_pair[0][0], image_cp.shape[0] + point_pair[0][1] * -1)
         pair_end = (point_pair[1][0], image_cp.shape[0] + point_pair[1][1] * -1)
-        image_cp = cv2.line(image_cp, pair_start, pair_end, (19, 93, 30), thickness=1, lineType=cv2.LINE_AA)
+        r = int(np.random.randint(0, 39, 1)[0])
+        g = int(np.random.randint(70, 110, 1)[0])
+        b = int(np.random.randint(10, 50, 1)[0])
+        image_cp = cv2.line(image_cp, pair_start, pair_end, (r, g, b), thickness=1, lineType=cv2.LINE_AA)
     return image_cp
+
+def draw_bubbles(image):
+    bubble_num = 500
+    height = image.shape[0]
+    width = image.shape[1]
+    x_points = np.random.randint(0, width, (bubble_num, 1))
+    y_points = np.random.randint(0, height, (bubble_num, 1))
+    center = np.concatenate([x_points, y_points], 1)
+    axes_short = np.random.randint(1, 3, (bubble_num, 1))
+    axes_long = np.random.randint(3, 5, (bubble_num, 1))
+    axes = np.concatenate([axes_long, axes_short], 1)
+    angle = np.random.normal(90, 6, bubble_num)
+    colors = np.random.normal(170, 10, (bubble_num, 3)).astype(np.int32)
+    colors.clip(0, 255)
+    for i in range(bubble_num):
+        color = (int(colors[i, 0]), int(colors[i, 2]), int(colors[i, 2]))
+        image = cv2.ellipse(image, tuple(center[i]), tuple(axes[i]), angle[i], 0, 360, color, thickness=-1)
+        
+    return image
 
 def main():
     # load images
-    image_paths, _ = read_files()
+    image_paths, label_paths = read_files()
     images = []
     for path in image_paths:
         images.append(cv2.imread(path))
 
     # calculate tree points
-    points = calc_tree_points((400, 20), 50.0, 90.0, 8)
+    points_tree1 = calc_tree_points((340, 0), 50.0, 90.0, 8)
+    points_tree2 = calc_tree_points((340, 180), 50.0, 90.0, 8)
+    points_tree = np.concatenate([points_tree1, points_tree2])
     # points: [[[start_x, start_y], [end_x, end_y]], [[start_x, start_y], [end_x, end_y]], ...]
 
     # draw tree
     big_image = cv2.resize(images[0], dsize=None, fx=2.0, fy=2.0)
-    big_image = draw_lines(big_image, points)
+    big_image = draw_lines(big_image, points_tree)
+    big_image = draw_bubbles(big_image)
     image = cv2.resize(big_image, dsize=None, fx=0.5, fy=0.5)
 
     # write image
-    cv2.imwrite(image_paths[0].replace(".jpg", "_tree.jpg"), image)
+    #cv2.imwrite(image_paths[0].replace(".jpg", "_tree.jpg"), image)
     
     # For sample image
     cv2.imwrite("/workspace/scripts/data_augmentation/sample/original.jpg", images[0])
-    cv2.imwrite("/workspace/scripts/data_augmentation/sample/add_tree.jpg", image)
+    cv2.imwrite("/workspace/scripts/data_augmentation/sample/add_object.jpg", image)
 
 
 if __name__ == "__main__":
